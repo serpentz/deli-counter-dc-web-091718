5ca3d3f3dd88a84621adf5d38cf2b25d750a1abd
Absalom Cherinet <absalomcherinet@gmail.com>
2018-09-01 22:06:13 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,18 @@
 # Write your code here.
+def line(queue)
+  queue.size >0 ? (puts "The line is currently:"+ line_sequence(queue) ): (puts "The line is currently empty.")
+end
+def line_sequence(queue)
+    string = ""
+    queue.map.with_index {|person,place_in_line|
+        string << " #{place_in_line+1}. #{person}"
+    }
+    string
+end
+def take_a_number(queue, customer)
+  queue.push(customer)
+  puts "Welcome, #{customer}. You are number #{queue.index(customer) + 1} in line."
+end
+def now_serving(queue)
+queue.size>0 ? (puts "Currently serving #{queue.shift}." ):(puts "There is nobody waiting to be served!")
+end
