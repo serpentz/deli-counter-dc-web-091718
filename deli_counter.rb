78c526361a845a355ebcc0fdbfef01f9755fadfd
Absalom Cherinet <absalomcherinet@gmail.com>
2018-09-01 21:42:26 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,11 @@
 # Write your code here.
 def line(queue)
-  queue.size >0 ? {"The line is currently: "} : puts "should say the line is empty"
+  queue.size >0 ? "The line is currently: "+ line_sequence(queue) : puts "should say the line is empty"
+end
+def line_sequence(queue)
+    string = ""
+    queue.map.with_index {|person,place_in_line|
+        string << "#{place_in_line+1}. person"
+    }
+    string
 end
